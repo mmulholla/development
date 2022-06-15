c7e2a7883f70a11f61423998726ed85841fc6042
Martin Mulholland <mmulholl@redhat.com>
2022-06-15 13:06:12 -0400

    improve message

--- scripts/src/checkprcontent/checkpr.py
+++ scripts/src/checkprcontent/checkpr.py
@@ -129,7 +129,7 @@ def ensure_only_chart_is_modified(api_url, repository, branch):
                 if matches_found == 1:
                     pattern_match = match
                 elif pattern_match.groups() != match.groups():
-                    msg = f"[ERROR] PR must only include one chart"
+                    msg = f"[ERROR] A PR must contain only one chart. Current PR includes files for multiple charts."
                     print(msg)
                     print(f"::set-output name=pr-content-error-message::{msg}")
                     sys.exit(1)
